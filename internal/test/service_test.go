1bc2e06c4479496a3e19f777d84574de3d177b99
Heater <belckbird19@gmail.com>
2024-07-15 19:55:02 +0300

    finished

--- internal/test/service_test.go
+++ internal/test/service_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/Heatdog/VkML/internal/services"
 	"github.com/Heatdog/VkML/internal/storage/postgre"
 	"github.com/Heatdog/VkML/internal/storage/redis"
+	"github.com/go-redis/redismock/v9"
 	"github.com/pashagolub/pgxmock/v3"
+	lib_redis "github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,6 +23,9 @@ func TestProcess(t *testing.T) {
 	}
 	defer dbMock.Close()
 
+	cacheDb, mock := redismock.NewClientMock()
+	defer cacheDb.Close()
+
 	opt := &slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelError,
@@ -29,7 +34,7 @@ func TestProcess(t *testing.T) {
 	slog.SetDefault(logger)
 
 	storage := postgre.New(dbMock, logger)
-	cache := redis.New()
+	cache := redis.New(cacheDb, logger)
 	documentProcess := services.New(storage, cache, logger)
 
 	type mockBehavior func(inDocument models.Document, outDocument *models.Document, err error)
@@ -42,7 +47,7 @@ func TestProcess(t *testing.T) {
 		err         error
 	}{
 		{
-			name: "ok",
+			name: "ok get from database",
 			inDocument: models.Document{
 				URL:       "/set",
 				PubDate:   12345,
@@ -62,6 +67,14 @@ func TestProcess(t *testing.T) {
 					inDocument.PubDate, inDocument.FetchTime, inDocument.Text).
 					WillReturnResult(pgxmock.NewResult("INSERT", 1))
 
+				mock.ExpectZAdd(inDocument.URL, lib_redis.Z{
+					Member: &inDocument,
+					Score:  float64(inDocument.FetchTime),
+				}).SetVal(1)
+
+				mock.ExpectZRangeWithScores(inDocument.URL, 0, 0).
+					SetVal([]lib_redis.Z{})
+
 				rowMin := pgxmock.NewRows([]string{"pub_date", "fetch_time", "text"})
 				rowMin.AddRow(outDocument.PubDate, outDocument.FirstFetchTime,
 					"hello world")
@@ -72,6 +85,9 @@ func TestProcess(t *testing.T) {
 					WHERE url`).
 					WithArgs(inDocument.URL).WillReturnRows(rowMin)
 
+				mock.ExpectZRevRangeWithScores(inDocument.URL, 0, 0).
+					SetVal([]lib_redis.Z{})
+
 				rowMax := pgxmock.NewRows([]string{"pub_date", "fetch_time", "text"})
 				rowMax.AddRow(uint64(45544), outDocument.FetchTime, outDocument.Text)
 
@@ -81,6 +97,50 @@ func TestProcess(t *testing.T) {
 					WHERE url`).
 					WithArgs(inDocument.URL).WillReturnRows(rowMax)
 
+			},
+		},
+		{
+			name: "ok get from cache",
+			inDocument: models.Document{
+				URL:       "/set",
+				PubDate:   12345,
+				FetchTime: 45567,
+				Text:      "1234",
+			},
+			outDocument: &models.Document{
+				URL:            "/set",
+				PubDate:        12300,
+				FetchTime:      99999,
+				Text:           "5668",
+				FirstFetchTime: 12300,
+			},
+			err: nil,
+			mockFunc: func(inDocument models.Document, outDocument *models.Document, err error) {
+				dbMock.ExpectExec("INSERT INTO documents").WithArgs(inDocument.URL,
+					inDocument.PubDate, inDocument.FetchTime, inDocument.Text).
+					WillReturnResult(pgxmock.NewResult("INSERT", 1))
+
+				mock.ExpectZAdd(inDocument.URL, lib_redis.Z{
+					Member: &inDocument,
+					Score:  float64(inDocument.FetchTime),
+				}).SetVal(1)
+
+				mock.ExpectZRangeWithScores(inDocument.URL, 0, 0).
+					SetVal([]lib_redis.Z{
+						{
+							Member: models.Document{
+								PubDate:   outDocument.PubDate,
+								FetchTime: outDocument.FirstFetchTime,
+							},
+						},
+					})
+
+				mock.ExpectZRevRangeWithScores(inDocument.URL, 0, 0).
+					SetVal([]lib_redis.Z{
+						{
+							Member: *outDocument,
+						},
+					})
 			},
 		},
 		{
@@ -130,6 +190,11 @@ func TestProcess(t *testing.T) {
 					inDocument.PubDate, inDocument.FetchTime, inDocument.Text).
 					WillReturnResult(pgxmock.NewResult("INSERT", 1))
 
+				mock.ExpectZAdd(inDocument.URL, lib_redis.Z{
+					Member: &inDocument,
+					Score:  float64(inDocument.FetchTime),
+				}).SetVal(1)
+
 				dbMock.ExpectQuery(`
 					SELECT pub_date, fetch_time, text 
 					FROM documents 
@@ -151,6 +216,11 @@ func TestProcess(t *testing.T) {
 				dbMock.ExpectExec("INSERT INTO documents").WithArgs(inDocument.URL,
 					inDocument.PubDate, inDocument.FetchTime, inDocument.Text).
 					WillReturnResult(pgxmock.NewResult("INSERT", 1))
+
+				mock.ExpectZAdd(inDocument.URL, lib_redis.Z{
+					Member: &inDocument,
+					Score:  float64(inDocument.FetchTime),
+				}).SetVal(1)
 
 				rowMin := pgxmock.NewRows([]string{"pub_date", "fetch_time", "text"})
 				rowMin.AddRow(uint64(123), uint64(123), "hello world")
